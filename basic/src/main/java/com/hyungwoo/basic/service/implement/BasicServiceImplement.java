b28411adbd183433f5b37135921224ac0baf82bb
Chixol <phw9278@gmail.com>
2024-03-25 14:51:18 +0900

    2024-03-25 14:00

---
+++ basic/src/main/java/com/hyungwoo/basic/service/implement/BasicServiceImplement.java
@@ -0,0 +1,12 @@
+package com.hyungwoo.basic.service.implement;
+
+import com.hyungwoo.basic.service.Basicservice;
+
+public class BasicServiceImplement implements Basicservice{
+
+    @Override
+    public String getHello() {
+        return "Hello Springboot!!!";
+    }
+    
+}
