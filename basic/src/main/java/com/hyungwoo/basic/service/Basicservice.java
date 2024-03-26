f79d385e4143647e3955889aeaef27aad64cde05
Chixol <phw9278@gmail.com>
2024-03-26 14:52:06 +0900

    2024-03-26 14:00

--- basic/src/main/java/com/hyungwoo/basic/service/Basicservice.java
+++ basic/src/main/java/com/hyungwoo/basic/service/Basicservice.java
@@ -3,7 +3,7 @@ package com.hyungwoo.basic.service;
 public interface BasicService {
     // interface의 메서드는 반드시 public abstract 이여야 함
     // public abstract String getHello();
- 
+
     // public abstract 제어자는 생략해도 됨
     String getHello();
     String getApple();
