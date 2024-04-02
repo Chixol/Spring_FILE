663dd3d7cdf1d613c5a8eb3d8fdb56df6fbd23db
Chixol <phw9278@gmail.com>
2024-04-02 15:07:06 +0900

    2024-04-02 14:00

--- board/src/main/java/com/example/board/controller/UserController.java
+++ board/src/main/java/com/example/board/controller/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/vi/auth")
-public class AuthController {
+@RequestMapping("/api/vi/user")
+public class UserController {
     
 }
