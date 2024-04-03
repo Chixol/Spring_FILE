02af3f3f4c6e933d66ca9ca35a61347ccf64c7a9
Chixol <phw9278@gmail.com>
2024-04-03 15:11:29 +0900

    2024-04-03 14:00

--- board/src/main/java/com/example/board/service/implementations/UserServiceImplementation.java
+++ board/src/main/java/com/example/board/service/implementations/UserServiceImplementation.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.board.dto.response.ResponseDto;
 import com.example.board.dto.response.user.GetUserResponseDto;
+import com.example.board.entity.UserEntity;
 import com.example.board.repository.UserRepository;
 import com.example.board.service.UserService;
 
@@ -20,16 +21,26 @@ public class UserServiceImplementation implements UserService {
     public ResponseEntity<? super GetUserResponseDto> getUser(String email) {
         
         try {
-
-            return ResponseDto.notExistUser();
-            
+            // 1. User 테이블에서 email에 해당하는 유저 조회
+            // SELECT * FROM user WHERE email = :email
+            // findByEmail(email)
+            // (email -> (조회 결과 인스턴스))
+            UserEntity userEntity = userRepository.findByEmail(email); 
+    
         } catch (Exception exception) {
-            exception.printStackTrace();
+
+        // 1-1. 조회 처리 중 데이터베이스 관련 예외가 발생하면 데이터베이스 에러 응답 처리 (try-catch)
+            exception.printStackTrace(); //터미널에 어떤 에러가 떳는지 확인하는 쿼리
             return ResponseDto.databaseError();
         }
 
-        return GetUserResponseDto.success("email@email.com", "홍길동", null);
 
+        // 2. 조회 결과에 따라 반환 결정
+        // 1) false이면 존재하지 않는 유저 응답처리 x (true, false 형태면 효율적이지 않음)
+        // 2) null 이면 존재하지 않는 유저 응답처리 
+
+        // 3. 조회 결과 데이터를 성공 응답
     }
+    
     
 }
